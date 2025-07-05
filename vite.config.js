9f1e0facdc57acdd7533b4ae324b4b80f42cbdef
NagargojeOmkar <Konomkar@gmail.com>
2025-07-06 01:53:09 +0530

    Upload working project with HashRouter + Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-})
+  build: {
+    outDir: 'dist',  // Explicit output directory
+    rollupOptions: {
+      input: {
+        main: './index.html'  // Explicit entry point
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    strictPort: true
+  }
+});
